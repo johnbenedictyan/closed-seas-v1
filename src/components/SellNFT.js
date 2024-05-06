0e809afeeeac7fdb3d6534a0284b4b3f061fe6de
John Benedict Yan <johnbenedictyan@u.nus.edu>
2024-05-06 16:33:34 +0800

    update logos and format files

--- src/components/SellNFT.js
+++ src/components/SellNFT.js
@@ -1,8 +1,7 @@
-import Navbar from "./Navbar";
 import { useState } from "react";
-import { uploadFileToIPFS, uploadJSONToIPFS } from "../pinata";
 import Marketplace from "../Marketplace.json";
-import { useLocation } from "react-router";
+import { uploadFileToIPFS, uploadJSONToIPFS } from "../pinata";
+import Navbar from "./Navbar";
 
 export default function SellNFT() {
   const [formParams, updateFormParams] = useState({
@@ -13,7 +12,6 @@ export default function SellNFT() {
   const [fileURL, setFileURL] = useState(null);
   const ethers = require("ethers");
   const [message, updateMessage] = useState("");
-  const location = useLocation();
 
   async function disableButton() {
     const listButton = document.getElementById("list-button");
